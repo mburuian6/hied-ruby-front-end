4fbee24e6b6fddb009e480d2cd3b8259bda90ff0
Ian Mburu <mburu@finplusgroup.com>
2022-01-30 06:49:35 +0300

    IndexDB

--- src/helpers.js
+++ src/helpers.js
@@ -6,3 +6,24 @@ export const isPersistedState = stateName =>{
 export const timeFormat = startTime => {
     return new Date(startTime).toLocaleDateString()
 }
+const isIndexDbSupported = () => {
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return false;
+  }
+  return true;
+}
+
+// export const openIndexDB = () => {
+//   'use strict';
+//   if (isIndexDbSupported()){
+//     var dbPromise = idb.open('hied',1, (upgradeDb) => {
+//       if (!upgradeDb.objectStoreNames.contains('posts')) {
+//         upgradeDb.createObjectStore(
+//             'posts',
+//             {autoIncrement: true}
+//         );
+//       }
+//     });
+//   }
+// }
