5e2d114b0b527b5bac7dc5fc98a6eaccae7c8241
Ian Mburu <mburu@finplusgroup.com>
2022-01-30 06:46:56 +0300

    Refactor JobList to fetch from network first

--- src/JobList/JobList.js
+++ src/JobList/JobList.js
@@ -1,87 +1,84 @@
-import React, {Component} from 'react';
-import JobForm from '../JobForm/JobForm';
+import React, { useEffect, useState } from 'react';
 import JobItem from '../JobItem/JobItem';
-import { Container, Grid } from '@mui/material';
-import { API_JOBS_URL } from '../config';
+import { Grid, Button } from '@mui/material';
+import { API_JOBS_URL, API_JOBS_PATH } from '../config';
+import { defaultInstance as axios} from '../axiosConfig';
+import toast from '../FlashNotification/FlashNotification';
 
-const api_url = API_JOBS_URL ;
+const JobList = () => {
+  const[items, setItems] = useState([]);
+  var networkDataReceived = false;
+  var response_items;
 
-class JobList extends Component{
+  useEffect(()=>{
+    getJobs();
+  },[]);
 
-    constructor(props){
-        super(props)
-
-        this.state = {
-            items:[]
-        }
-
-        this.updateJobList = this.updateJobList.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
-    }
-
-    componentDidMount(){
-        this.getJobs();
-    }
-
-    getJobs(){
-        fetch(api_url)
-          .then(response => response.json())
-          .then(response_items => {
-              this.setState({
-                  items: response_items.reverse()
-              })
-          });
-    }
-
-    updateJobList(item){
-        let _items = this.state.items;
-        _items.unshift(item);
-        this.setState({
-            items: _items
+  const getJobs = () => {
+    axios.get(API_JOBS_PATH).then((response) => {
+      response_items = response.data;
+      setItems(response_items["_embedded"]["posts"]?.reverse());
+    })
+    .catch((error) => {
+        //network failed for some reason, get cached content
+        caches.match(API_JOBS_URL)
+        .then (response => {
+          if(!response){
+            retryJobs();
+            return;
+          }
+          return response.json()
         })
-    }
-
-    deleteItem(item){
-        var deleteUrl = api_url+`/${item.id}`;
-
-        fetch(deleteUrl,{
-            method:"DELETE"
-        }).then(() => {
-            var _items = this.state.items;
-            var index = _items.indexOf(item);
-
-            _items.splice(index,1);
-            this.setState({
-                items: _items
-            })
+        .then(response_json => {
+          if(response_json){
+            setItems(response_json._embedded.posts?.reverse());
+            toast.warning('Your device is offline')
+          }
         })
+      })
     }
 
-    render(){
-        return (
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                <Grid item xs={12}>
-                  <JobForm api_url={api_url} 
-                    updateJobList={this.updateJobList}/>
-                </Grid>
-                
-                
-                    {this.state.items
-                      .map((item) => (
-                        <Grid item xs={2} sm={4} md={4} key={item.id} >
-                          <JobItem                             
-                            job={item}
-                            jobId= {item.id}
-                            deleteItem={this.deleteItem}
-                          />
-                        </Grid>
-                      ))}
-                
-            </Grid>
-        )
-    }
+  const retryJobs = () => {
+    axios.get(API_JOBS_PATH)
+      .then((response) => {
+          response_items = response.data;
+          setItems(response_items["_embedded"]["posts"]?.reverse());
+        })
+      .catch((error) => {
+          console.log(error);
+          if(error.toString().search('ERR_CONNECTION_REFUSED')){
+            toast.error("Error! Check your internet connection and retry.")
+          }
+          else{
+            toast.error("Error! Contact Admin.")
+          }
+        })
+  }
 
+ 
+  return (
+    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid item xs={3}>
+        <Button variant="contained" href="/jobform" >
+          New Post
+        </Button> 
+      </Grid>
+              
+      {items.map((item, index) => (
+        <Grid item xs={2} sm={4} md={4} key={index} >
+          <JobItem
+          job={item}
+          jobIndex = {index}
+          jobId={item.id}
+          jobLink = {item['_links']['self']}
+          />
+        </Grid>
+          ))}
+
+      </Grid>
+  )
 }
 
 export default JobList;
+
 
