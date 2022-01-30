b8b25ce61fab21ca6ddc2738b945db7109614e4a
Ian Mburu <mburu@finplusgroup.com>
2022-01-30 07:05:07 +0300

    Remove Legacy code

--- src/BidList/BidList.js
+++ src/BidList/BidList.js
@@ -12,56 +12,9 @@ import toast from '../FlashNotification/FlashNotification';
 
 const BidList = ({ job }) => {
  
-  // const {state: bids, loading_bids, error_bids } = useBidsFetch( jobId );
-  // let _bids = Object.values(bids);
-  // const forceUpdate = useForceUpdate();
   
   const acceptBid = async (bid) => {
-  //accept bid and 
-  // delete all the rest
-  //mark post as closed
-
-  // var updateBidUrl = `${BASE_URL}jobs/${jobId}/bids/${bid.id}`;
-  // var deleteBidsUrl = `${BASE_URL}jobs/${jobId}/bids/`;
-  // var closePostUrl = `${BASE_URL}jobs/${jobId}`;
-
-  var bid_data = {
-      pay: bid.pay,
-      comment:bid.comment,
-      picked: true 
-  }
-
-  // var job_data = {
-  //     title: job.title,
-  //     description: job.description,
-  //     pay: job.pay,
-  //     start: job.start,
-  //     open: false
-  // }
-
-  // fetch(closePostUrl,{
-  //   method: "PUT",
-  //   headers:{'Content-Type': 'application/json'},
-  //   body: JSON.stringify(job_data)
-  // }).then(() => {
-  //   fetch(updateBidUrl,{
-  //   method: "PUT",
-  //   headers:{'Content-Type': 'application/json'},
-  //   body: JSON.stringify(bid_data)
-  //   })
-  // }).then(() => {
-  //   _bids.filter(b => {
-  //     return b.id != bid.id
-  //   }).map(b => {
-  //     fetch(deleteBidsUrl+`${b.id}`,
-  //     {method: 'DELETE'})
-  //   })
-  // }).then(() => {
-  //   sessionStorage.removeItem(jobId);
-  //   return(
-  //     <Navigate to="/notice-board" />
-  //   )
-  // })
+  
 
   }
 
