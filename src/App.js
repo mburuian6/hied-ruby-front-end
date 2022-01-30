31f03f349479e4cf3717b8df37eb8db607632b7d
Ian Mburu <mburu@finplusgroup.com>
2022-01-30 06:55:43 +0300

    Add component links

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './Header/Header';
 import Home from './Home/Home';
-import JobPost from './JobPost/JobPost';
+import JobView from './JobView/JobView';
 import NotFound from './NotFound/NotFound';
 import NoticeBoard from './NoticeBoard/NoticeBoard';
+import SignUp from './Auth/SignUp';
+import Login from './Auth/Login';
+import JobForm from './JobForm/JobForm';
+
+// const axios = require('axios').default;
 
 const App = () => {
   return(
@@ -14,8 +19,12 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/:jobId' element={<JobPost/>} />
+        <Route path='/:jobId' element={<JobView/>} />
         <Route path='/notice-board' element={< NoticeBoard />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/jobform' element={<JobForm />} />
+        <Route path='/jobview' element={<JobView />} />
         <Route path='/*' element={<NotFound/>} />
         
       </Routes>
