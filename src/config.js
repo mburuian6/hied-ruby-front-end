1a1bd32139bedd34646db7a7a23c1eeed2f6cd50
Ian Mburu <mburu@finplusgroup.com>
2022-02-14 06:24:36 +0300

    Add login and sign up paths

--- src/config.js
+++ src/config.js
@@ -2,10 +2,13 @@ import { isPersistedState } from "./helpers";
 
 const BASE_URL = 'http://localhost:8080/';
 const API_HOME_URL = BASE_URL;
-const API_JOBS_URL = 'http://localhost:8080/posts';
-const API_SIGN_UP_URL = 'http://localhost:8080/auth/signup';
-const API_LOGIN_URL = 'http://localhost:8080/auth/login';
 
+//Auth
+const API_SIGN_UP_PATH = '/users';
+const API_LOGIN_PATH = '/oauth/token';
+const API_LOGOUT_PATH = '/oauth/revoke';
+
+//Content
 const API_JOBS_PATH = '/posts';
 const API_POST_BIDS_PATH = '/bids/open_post_bids';
 const API_BIDS_PATH = '/bids';
@@ -20,9 +23,8 @@ headers.append('Authorization',
 export {
     BASE_URL, 
     API_HOME_URL,
-    API_JOBS_URL,
-    API_SIGN_UP_URL,
-    API_LOGIN_URL,
+    API_SIGN_UP_PATH,
+    API_LOGIN_PATH,
     headers,
     API_JOBS_PATH,
     API_BIDS_PATH,
