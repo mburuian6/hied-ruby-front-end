04d2793c8ba27c43ee515a84819dff03f3cdfbe1
Ian Mburu <mburu@finplusgroup.com>
2022-03-24 15:04:54 +0300

    Header formatting

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,21 +8,37 @@ const Header = () => {
   const HasLoggedIn = () => {
     return (
       <>
-        <Grid container spacing={2} justifyContent="flex-end">
+        {/* <Grid container spacing={2} justifyContent="flex-end">
           <Grid item>
             Logged in:
             <Link to="" variant="body2">
               {isPersistedState("username")}
             </Link>
           </Grid>
-        </Grid>
+        </Grid> */}
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Logged In: {isPersistedState("username")}
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/logout">
+                Log out
+              </a>
+            </li>
+          </ul>
+        </div>
       </>
     );
   };
 
   const HasNotLoggedIn = () => {
     return (
-      <Grid container spacing={2} justifyContent="flex-end">
+      <>
+        {/* <Grid container spacing={2} justifyContent="flex-end">
         <Grid item>
           <Link to="/signup" variant="body2">
             Sign Up
@@ -34,7 +50,23 @@ const Header = () => {
             Login
           </Link>
         </Grid>
-      </Grid>
+      </Grid> */}
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/signup">
+                Sign Up
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/login">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </>
     );
   };
 
@@ -47,20 +79,43 @@ const Header = () => {
 
   return (
     <>
-      <Grid container>
-        <Grid item>
-        {/* <Headroom> */}
-          <Link to="/">
-            <header>
-              <Typography variant="h3" color="black">
-                Hi'ed
-              </Typography>
-            </header>
-          </Link>
-          {/* </Headroom> */}
-        </Grid>
-      </Grid>
-      <AppHeader />
+      <nav class="navbar navbar-expand-lg ">
+        <div class="container px-5">
+          <a class="navbar-brand" href="/">
+            <Typography variant="h3" color="blue">
+              Hi'ed
+            </Typography>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <AppHeader />
+        </div>
+      </nav>
+
+      {/* <Grid container> */}
+      {/* <Grid item> */}
+
+      {/* <Link to="/"> */}
+      {/* <header> */}
+      {/* <Typography variant="h3" color="black"> */}
+      {/* Hi'ed */}
+      {/* </Typography> */}
+      {/* </header> */}
+      {/* </Link> */}
+
+      {/* </Grid> */}
+      {/* </Grid> */}
+      {/* <AppHeader />  */}
     </>
   );
 };
