1ad991a2b1a83b6ebb061c80afe49a61af73a221
Ian Mburu <mburu@finplusgroup.com>
2022-03-24 15:03:15 +0300

    Logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './css/header.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './Header/Header';
@@ -11,6 +12,7 @@ import SignUp from './Auth/SignUp';
 import Login from './Auth/Login';
 import JobForm from './JobForm/JobForm';
 import ProtectedRoute from './ProtectedRoute/ProtectedRoute';
+import Logout from './Auth/Logout';
 
 // const axios = require('axios').default;
 
@@ -23,6 +25,7 @@ const App = () => {
         <Route path='/notice-board/:owner' element={<ProtectedRoute>< NoticeBoard /></ProtectedRoute>} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/logout' element={<Logout />} />
         <Route path='/jobform' element={<ProtectedRoute><JobForm /></ProtectedRoute>} />
         <Route path='/jobview' element={<ProtectedRoute><JobView /></ProtectedRoute>} />
         <Route path='/*' element={<NotFound/>} />
