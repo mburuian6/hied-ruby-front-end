5efdc74651697578f2483fb524bef3c6509d4674
Ian Mburu <mburu@finplusgroup.com>
2022-04-04 07:28:01 +0300

    Add avatar and badge

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,27 +1,82 @@
 import React from "react";
 import {
-  AppBar,
+  AppBar, Avatar, Badge,
   Box,
-  Button,
+  Button, Divider, MenuItem,
   Toolbar,
   Typography,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import { isLoggedIn, persistedState } from "../helpers";
 import IconButton from "@mui/material/IconButton";
-import { useNavigate } from "react-router-dom";
+import Menu from '@mui/material/Menu';
+import MailIcon from '@mui/icons-material/Mail';
 
 const Header = () => {
-  const [loggedIn, setLoggedIn] = React.useState(isLoggedIn());
-  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [badgeContent, setBadgeContent] = React.useState(20);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const HasLoggedIn = () => {
     return (
       <>
-        <Button color="inherit">
-          {persistedState("username").charAt(0)}
-        </Button>
+        {/*<Button color="inherit">*/}
+        {/*  {persistedState("username").charAt(0)}*/}
+        {/*</Button>*/}
+
+        <div>
+          <Badge
+            badgeContent={badgeContent}
+            color="secondary"
+            max={10}
+            sx={{ width: 30, height: 30 }}
+            variant={'dot'}
+          >
+            <MailIcon />
+          </Badge>
+
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <Avatar
+              sx={{ width: 30, height: 30 }}
+              alt="User">
+              {persistedState("username").charAt(0).toUpperCase()}
+            </Avatar>
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem>{persistedState("username")}</MenuItem>
+            <Divider/>
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
+        </div>
       </>
     );
   };
@@ -41,26 +96,6 @@ const Header = () => {
     }
     return <HasNotLoggedIn />;
   };
-
-  // return (
-  //   <>
-  //
-  //     <Grid container>
-  //     <Grid item>
-  //
-  //     <Link to="/">
-  //     <header>
-  //     <Typography variant="h3" color="black">
-  //     Hi'ed
-  //     </Typography>
-  //     </header>
-  //     </Link>
-  //
-  //     </Grid>
-  //     </Grid>
-  //     <AppHeader />
-  //   </>
-  // );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
