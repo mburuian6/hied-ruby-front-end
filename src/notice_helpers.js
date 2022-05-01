cad4e85fb677e0c599380e7c47e389cfe6fa5e62
Ian Mburu <mburu@finplusgroup.com>
2022-05-01 17:24:52 +0300

    Break down large method for long messages construction

--- src/notice_helpers.js
+++ src/notice_helpers.js
@@ -1,23 +1,9 @@
 
 // TODO: Add user links
-export const getLongMessage = (notification) => {
-  const subject = getSubject(notification.type);
-  let message = '';
-  if (notification.type === 'bid_accepted'){
-    message = `Dear ${notification.username}, your bid for post ${notification.data.post} ` /
-      `(${notification.data.post_link}) has been accepted at ${notification.data.time}. Please contact the ` /
-      `user[USER-LINK] for more details`
-  } else if (notification.type === 'bid_rejected'){
-    message = `Dear ${notification.username}, your bid for post ${notification.data.post} ` /
-      `(${notification.data.post_link}) has been rejected at ${notification.data.time}. Good luck next time!`
-  } else if (notification.type === 'post_accepted_bid'){
-    message = `Dear ${notification.username}, you have accepted the bid ${notification.data.bid}: ` /
-      `Kes ${notification.data.bid_pay} for post ${notification.data.post}(${notification.data.post_link}) from ` /
-      `${notification.data.bid_user}[USER-LINK] at ${notification.data.time}. Please contact the user for more details`
-  } else {
-    message = `Dear ${notification.username} please note this message.`
-  }
 
+export const getLongMessage = (notification) => {
+  const subject = getSubject(notification.notification_type);
+  var message = constructLongMessage(notification);
   return {subject:subject,message:message}
 }
 
@@ -53,4 +39,22 @@ const getSubject = (type) => {
   } else {
     return 'JUST FYI'
   }
+}
+
+const constructLongMessage = (notification) => {
+  let formatted_time = timeFormat(notification.data.time)
+  if(notification.notification_type === 'bid_accepted'){
+    return `Dear ${notification.username}, your bid for post ${notification.data.post} 
+      (${notification.data.post_link}) has been accepted at ${formatted_time}. Please contact the 
+      user[USER-LINK] for more details`
+  } else if(notification.notification_type === 'bid_rejected'){
+    return `Dear ${notification.username}, your bid for post ${notification.data.post} 
+      (${notification.data.post_link}) has been rejected at ${formatted_time}. Good luck next time!`
+  } else if(notification.notification_type === 'post_accepted_bid'){
+    return `Dear ${notification.username}, you have accepted the bid ${notification.data.bid}: 
+      Kes ${notification.data.bid_pay} for post ${notification.data.post}(${notification.data.post_link}) from 
+      ${notification.data.bid_user}[USER-LINK] at ${formatted_time}. 
+      Please contact the user for more details`
+  }
+  return `Dear ${notification.username} please note this message.`
 }
