a8e3132c78204126482dbf7d08dedc154ea650cb
Ian Mburu <mburu@finplusgroup.com>
2022-05-02 17:16:09 +0300

    Form to display user details and update user[email only]

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -47,6 +47,49 @@ const Profile = () => {
   }
 
   return (
+    <form onSubmit={handleSubmit} id="user_profile_form" autoComplete="off">
+      <Stack spacing={3}>
+        <Grid item xs={12} sm={4}>
+          <TextField
+            label="Username"
+            variant="filled"
+            type="text"
+            value={user.username}
+            disabled={true}
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <TextField
+            label="Joined"
+            variant="filled"
+            type="text"
+            value={timeFormatHuman(user.created_at)}
+            disabled={true}
+          />
+        </Grid>
+        <Divider/>
+        <h2>Contact</h2>
+        <Grid item xs={12} sm={4}>
+          <TextField
+            id="email"
+            label="Email"
+            variant="filled"
+            type="text"
+            value={email}
+            name="email"
+            onChange={(event)=>{setEmail(event.target.value)}}
+            required
+          />
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <Button variant="contained" color="primary" type="submit" disabled={loading}>
+            {" "}Update Profile{" "}
+          </Button>
+        </Grid>
+      </Stack>
+    </form>
   );
+
 }
+
 export default Profile;
