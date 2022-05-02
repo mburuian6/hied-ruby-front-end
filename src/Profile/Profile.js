9ca232a5dd9ae81d9f845bd64bad6b5583e2c260
Ian Mburu <mburu@finplusgroup.com>
2022-05-02 17:15:10 +0300

    Track initial details

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,4 +1,8 @@
 const Profile = () => {
+  let [user, setUser] = useState();
+  let [email, setEmail] = useState('');
+  let [loading, setLoading] = useState(false);
+  let username = persistedState('username');
   return (
   );
 }
