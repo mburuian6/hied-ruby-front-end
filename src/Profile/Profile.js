0f04438f5286679f9853edf9871e6825b5f0100a
Ian Mburu <mburu@finplusgroup.com>
2022-05-02 17:15:22 +0300

    Get profile details on load

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -3,6 +3,24 @@ const Profile = () => {
   let [email, setEmail] = useState('');
   let [loading, setLoading] = useState(false);
   let username = persistedState('username');
+
+  let getProfile = () => {
+    axios.get(API_GET_USER_PATH,{
+      params: {
+        username: username.toString()
+      }
+    }).then((response) => {
+      let response_items = response.data;
+      setUser(response_items["_embedded"]["user"]);
+      setEmail(response_items._embedded.user.email);
+    }).catch(()=>{
+      toast.error('User profile unavailable.')
+    })
+  };
+
+  useEffect(()=>{
+    getProfile();
+  },[]);
   return (
   );
 }
