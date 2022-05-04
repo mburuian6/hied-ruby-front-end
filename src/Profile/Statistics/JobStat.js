1345f7775ab69f6e9b436826af0577e7847d0cdf
Ian Mburu <mburu@finplusgroup.com>
2022-05-04 09:01:25 +0300

    Job stats

---
+++ src/Profile/Statistics/JobStat.js
@@ -0,0 +1,34 @@
+import {Divider, ListItem, ListItemText, Typography} from "@mui/material";
+import React from "react";
+import {timeFormat, timeFormatHuman} from "../../helpers";
+
+const JobStat = ({ job }) => {
+
+  return (
+    <>
+      <ListItem alignItems="flex-start">
+        <ListItemText
+          primary={`${job.title} - ${job.pay}`}
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+               Created: {timeFormatHuman(job.created_at)}
+              </Typography>
+              Start: {timeFormat(job.start)} :-
+              {job.description}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+    </>
+  )
+
+}
+
+export default JobStat;
