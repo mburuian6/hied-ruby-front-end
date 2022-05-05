f0e99679f7b7312acfd09dbeade83b1994b53a98
Ian Mburu <mburu@finplusgroup.com>
2022-05-05 08:26:02 +0300

    Tabpanel

--- src/Profile/Statistics.js
+++ src/Profile/Statistics.js
@@ -83,13 +83,46 @@ const Statistics = ({ username }) => {
     )))
   };
 
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} >
-          <Tab label="Posts"  />
-          <Tab label="Accepted Bids" />
-          <Tab label="Rejected Bids" />
+          <Tab label="Posts" {...a11yProps(0)} />
+          <Tab label="Accepted Bids" {...a11yProps(0)} />
+          <Tab label="Rejected Bids" {...a11yProps(0)} />
         </Tabs>
       </Box>
 
