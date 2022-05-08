4cf2482b39753e04d8c497fd7f32d859ce8a367b
Ian Mburu <mburu@finplusgroup.com>
2022-05-08 07:25:03 +0300

    Redesign to use simple card

--- src/JobItem/JobItem.js
+++ src/JobItem/JobItem.js
@@ -1,104 +1,40 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { Link } from 'react-router-dom';
+import {timeFormatWithTimeZone, timeFormatHuman} from "../helpers";
+import {Link} from "react-router-dom";
+import {Button} from "@mui/material";
+import CardActions from "@mui/material/CardActions";
 
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-export default function JobItem( props ) {
-  const [expanded, setExpanded] = React.useState(false);
-  const thisJob = props.job;
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+export default function JobItem({ job }) {
   return (
-    <Card sx={{ maxWidth: 345, margin: 5 }}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe">
-            {props.job.username.charAt(0).toUpperCase()}
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title= {
-          <Link 
-            to = { '/jobview' }
-            state = {{ job: props.job }}
-          > 
-            {props.job.title}
-          </Link>          
-        }
-        subheader={new Date(props.job.start).toLocaleString()}
-      /> 
-      {/* <CardMedia component="img" height="194" image=""/> */}
+
+
+        //
+        //         by: {<Link to={}></Link>}
+
+
+    <Card>
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-            {props.job.description.substring(0, 31)} 
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {timeFormatHuman(job.created_at)}
+        </Typography>
+        <Typography variant="h5" component="div">
+          <Link to = { '/jobview' } state = {{ job: job }}>{job.title}</Link>
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {timeFormatWithTimeZone(job.start)}
+        </Typography>
+        <Typography variant="body2">
+          {job.description? job.description.substring(0, 31) : 'No more info right now.'}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
+      <CardActions>
+        <Button size="small" href={`/profile/${job.username}`}>{job.username}</Button>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>
-            { //Check if message failed
-              props.job.closed == false? <>Open</> : <>Closed</>
-            }
-          </Typography>
-          <Typography paragraph>
-            {props.job.description}
-          </Typography>
-          <Typography>
-            <i>
-                by: {<Link to={`/profile/${props.job.username}`}>{props.job.username}</Link>}
-            </i>
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
