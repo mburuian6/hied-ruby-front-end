3d599640ea53b39f99844671864bdfcc30e13828
Ian Mburu <mburu@finplusgroup.com>
2022-05-08 11:49:46 +0300

    Style with card
    - add withdraw button

--- src/BidItem/BidItem.js
+++ src/BidItem/BidItem.js
@@ -1,24 +1,37 @@
 import { Button, Typography } from '@mui/material';
 import React from 'react';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import {persistedState} from "../helpers";
 
 const BidItem = ({ bid, acceptBid }) =>{
+  const isOwner = (persistedState('username') === bid.username);
 
   const acceptBidLocal = () =>{
     acceptBid(bid);
   }
 
+  const WithdrawButton = () => {
+    return(<Button size="small"> Withdraw </Button>);
+  }
+
   return (
     <>
-      <hr/>
-      <Typography variant='subtitle2'> Bid by: <i>{bid.username} </i>
-      Pay: {bid.pay} <Button onClick={acceptBidLocal}> Accept </Button>
-      </Typography>
-      <Typography variant='caption'> {bid.picked? 'Accepted':''} </Typography>
-      <Typography variant='caption'>
-        {bid.notes? `${bid.username} says: `+bid.notes: ''}
-      </Typography>
-      
-      
+      <Card sx={{ height:0.7 }}>
+        <CardContent>
+          <Typography gutterBottom variant="subtitle1" component="div">
+            Kes {bid.pay} by: {bid.username}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {bid.notes? bid.notes: ''}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={acceptBidLocal}> Accept</Button>
+          {isOwner? null : <WithdrawButton/> }
+        </CardActions>
+      </Card>
     </>
   );
 }
