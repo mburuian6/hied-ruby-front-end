e6c30d1498285c0119439c2744e61e45a88321c1
Ian Mburu <mburu@finplusgroup.com>
2022-05-23 15:47:51 +0300

    Remove log

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -33,10 +33,6 @@ export default function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
 
     // email validation
     if(validator.isEmail(data.get('email'))){
