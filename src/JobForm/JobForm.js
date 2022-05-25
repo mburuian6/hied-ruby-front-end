a518b5d8c8b037b8bdf02caf4e1a3387caa87755
Ian Mburu <mburu@finplusgroup.com>
2022-05-25 19:21:34 +0300

    Better default values

--- src/JobForm/JobForm.js
+++ src/JobForm/JobForm.js
@@ -45,8 +45,8 @@ const JobForm = () => {
 
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState('-70.9');
-  const [lat, setLat] = useState('42.35');
+  const [lng, setLng] = useState('36.8246');
+  const [lat, setLat] = useState('-1.2890');
   const [zoom, setZoom] = useState(9);
   const marker = useRef(null);
 
